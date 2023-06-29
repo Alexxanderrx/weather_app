00ee4e05643135b15d836121e1c140ea45e8f644
Víctor Alexander Morales Lucero <alexmorales196@gmail.com>
2023-06-29 07:50:52 -0500

    prueba para resolver el problema(cod:'400')

--- app/page.jsx
+++ app/page.jsx
@@ -1,27 +1,46 @@
 "use client"
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styles from './page.module.css'
-
-// async function getData(url) {
-//   const response = await fetch(url);
-//   const data = await response.json();
-
-//   return data;
-// }
 
 // async
 export default function Home() {
   // const dataRecup = await getData("http://api.openweathermap.org/geo/1.0/direct?q=lima&limit=5&appid=b7b1b4492885348f44fdc6c0af7556ca");
-  // const dataWeather = await getData(`https://api.openweathermap.org/data/2.5/weather?lat=${dataRecup[0].lat}&lon=${dataRecup[0].lon}&appid=b7b1b4492885348f44fdc6c0af7556ca`);
-  // console.log(dataRecup);
+  const [dataRecup, setDataRecup] = useState(null);
+  const [dataWeather, setDataWeather] = useState(null);
+
+  // ${dataRecup[0].lat}
+  // ${dataRecup[0].lon}
+
+  // const dataWeather = getData(`https://api.openweathermap.org/data/2.5/weather?lat${dataRecup[0].lat}=&lon=${dataRecup[0].lon}&appid=b7b1b4492885348f44fdc6c0af7556ca`);
+  console.log(dataRecup);
+  console.log(dataWeather);
+  let cityLat = dataRecup && dataRecup[0].lat;
+  let cityLon = dataRecup && dataRecup[0].lon;
+  console.log(cityLat);
+  console.log(cityLon);
+
+  let urlW = `https://api.openweathermap.org/data/2.5/weather?lat=${cityLat}&lon=${cityLon}&appid=b7b1b4492885348f44fdc6c0af7556ca`
+  console.log(urlW);
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("http://api.openweathermap.org/geo/1.0/direct?q=lima&limit=5&appid=b7b1b4492885348f44fdc6c0af7556ca");
+      const data = await response.json();
+      setDataRecup(data);
+    }
+    getData();
+    // console.log(cityLat);
+    // console.log(cityLon);
+    async function getWeather() {
+      const responseW = await fetch(urlW);
+      const dataW = await responseW.json();
+      setDataWeather(dataW);
+    }
+    getWeather();
+  }, []);
   // console.log(dataWeather);
 
-  // async function getData(url) {
-  //   const response = await fetch(url);
-  //   const data = await response.json();
 
-  //   return data;
-  // }
+
 
   const [open, setOpen] = useState(true);
   const OpenUp = () => {
@@ -59,7 +78,7 @@ export default function Home() {
         {/* style={{ border: "1px solid white" }} */}
         <div className="container p-4 d-flex justify-content-between" >
           <button type="button" className="btn btn-secondary" onClick={OpenUp}>Search for places</button>
-          <button type="button" className="btn btn-secondary">
+          <button type="button" className="btn btn-secondary rounded-circle">
             <i className="bi bi-geo-alt-fill"></i>
             {/* <img src="./weatherResources/next.svg" className='img-fluid' alt='next' style={{ width: "10px" }} /> */}
           </button>
@@ -68,15 +87,8 @@ export default function Home() {
           border: "1px solid blue",
           overflow: "hidden",
         }}>
-          <img src="./weatherResources/Shower.png" className='img-fluid' alt='Shower.png' style={{
-            position: "absolute",
-            width: "150px"
-          }} />
-          <img src="./weatherResources/Cloud-background.png" alt='Cloud-background.png' style={{
-            width: "530px",
-            opacity: "0.1",
-            // overflow: "hidden",
-          }} />
+          <img src="./weatherResources/Shower.png" className={styles.imgWeaterBC} alt='Shower.png' />
+          <img src="./weatherResources/Cloud-background.png" className={styles.bgCloudBC} alt='Cloud-background.png' />
         </div>
         <div className='container d-flex flex-column justify-content-center align-items-center' style={{
           border: "1px solid blue",
@@ -99,14 +111,15 @@ export default function Home() {
           padding: "0",
           gap: "15px"
         }}>
-          <button type="button" className="btn btn-secondary">°C</button>
-          <button type="button" className="btn btn-secondary">°F</button>
+          <button type="button" className="btn btn-secondary rounded-circle">°C</button>
+          <button type="button" className="btn btn-secondary rounded-circle">°F</button>
         </div>
-        <div className='container d-flex justify-content-center align-items-center' style={{
+        <div className='container p-0 d-flex justify-content-between align-items-center ' style={{
           border: "1px solid green",
           width: "80%",
         }}>
-          <div className='container d-flex flex-column justify-content-center align-items-center' style={{
+
+          <days className='container m-0 d-flex flex-column justify-content-center align-items-center' style={{
             border: "1px solid red",
             width: "18%",
           }}>
@@ -116,7 +129,56 @@ export default function Home() {
               <p>16<span>°C</span></p>
               <p>11<span>°C</span></p>
             </div>
-          </div>
+          </days>
+
+          <days className='container m-0 d-flex flex-column justify-content-center align-items-center' style={{
+            border: "1px solid red",
+            width: "18%",
+          }}>
+            <p>Tomorrow</p>
+            <img src='' alt='' />
+            <div>
+              <p>16<span>°C</span></p>
+              <p>11<span>°C</span></p>
+            </div>
+          </days>
+
+          <days className='container m-0 d-flex flex-column justify-content-center align-items-center' style={{
+            border: "1px solid red",
+            width: "18%",
+          }}>
+            <p>Tomorrow</p>
+            <img src='' alt='' />
+            <div>
+              <p>16<span>°C</span></p>
+              <p>11<span>°C</span></p>
+            </div>
+          </days>
+
+          <days className='container m-0 d-flex flex-column justify-content-center align-items-center' style={{
+            border: "1px solid red",
+            width: "18%",
+          }}>
+            <p>Tomorrow</p>
+            <img src='' alt='' />
+            <div>
+              <p>16<span>°C</span></p>
+              <p>11<span>°C</span></p>
+            </div>
+          </days>
+
+          <days className='container m-0 d-flex flex-column justify-content-center align-items-center' style={{
+            border: "1px solid red",
+            width: "18%",
+          }}>
+            <p>Tomorrow</p>
+            <img src='' alt='' />
+            <div>
+              <p>16<span>°C</span></p>
+              <p>11<span>°C</span></p>
+            </div>
+          </days>
+
         </div>
         <div className='container p-0 d-flex flex-column justify-content-center align-items-center' style={{
           border: "1px solid green",
